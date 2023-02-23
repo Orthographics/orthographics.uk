6d59903b22e8a091fcd5cf58fa81f2e299d34c10
Daniel Shields <me@danielshields.uk>
2023-02-23 00:22:14 +0000

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <h1>Hello!</h1>
     <Background />
+    <Header />
 </template>
 
 <script setup lang="ts">
 import Background from './components/Background.vue'
+import Header from './components/Header.vue'
 </script>
