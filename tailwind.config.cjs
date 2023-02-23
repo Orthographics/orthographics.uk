40345f1703b02f79d639903f2773e9f383643774
Daniel Shields <me@danielshields.uk>
2023-02-23 00:21:18 +0000

    Add theme to tailwind conf

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                orthopurple: '#6A4692',
+            },
+        },
+        fontFamily: {
+            display: ['Rubik'],
+            body: ['Campaign'],
+        },
     },
     plugins: [],
 }
