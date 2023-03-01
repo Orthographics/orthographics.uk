1c361bc4c4e183443b864a578f3458f0c56e41b6
Daniel Shields <me@danielshields.uk>
2023-03-01 01:09:20 +0000

    Add wave effect to background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -17,7 +17,12 @@ const { x: mouseX, y: mouseY, sourceType } = useMouse({ type: 'page' })
 
 const GAP = 50
 const RAD = 3
-const MOUSE_WEIGHT = 2
+
+const WAVE_LENGTH = 300
+const WAVE_SPEED = 0.1
+const WAVE_WEIGHT = 1.2 // the wave will multiply RAD in the range 1 -> WAVE_WEIGHT
+
+const MOUSE_WEIGHT = 2 // the mouse will multiply RAD in the range 1 -> MOUSE_WEIGHT
 const MOUSE_RANGE = 400
 
 function smoothstep(k: number) {
@@ -25,7 +30,7 @@ function smoothstep(k: number) {
     return 3 * kk ** 2 - 2 * kk ** 3
 }
 
-function draw() {
+function draw(ts: DOMHighResTimeStamp) {
     if (!canvas.value) return
 
     canvas.value.width = canvas.value.offsetWidth
@@ -37,22 +42,41 @@ function draw() {
     ctx.fillStyle = '#6a469222'
 
     for (let i = 0; i < canvas.value.height / GAP; i++) {
-        const cY = (i + 0.5) * GAP // - (scrollY.value % GAP)
+        const cY = (i + 0.5) * GAP
+
+        // RAD affected by y-axis wave
+        const rowSize =
+            RAD *
+            (WAVE_WEIGHT +
+                (1 - WAVE_WEIGHT) *
+                    Math.sin(
+                        ((2 * Math.PI) / WAVE_LENGTH) * (cY - ts * WAVE_SPEED)
+                    ))
 
         for (let j = 0; j < width.value / GAP; j++) {
             const cX = (j + 0.5) * GAP
 
-            const mouseDist = Math.sqrt(
-                (cX - mouseX.value) ** 2 + (cY - mouseY.value) ** 2
-            )
+            let size = rowSize
 
-            const size =
-                sourceType.value === 'mouse'
-                    ? RAD *
-                      (1 +
-                          (MOUSE_WEIGHT - 1) *
-                              (1 - smoothstep(mouseDist / MOUSE_RANGE)))
-                    : RAD
+            // Add effect of x-axis wave
+            size *=
+                WAVE_WEIGHT +
+                (1 - WAVE_WEIGHT) *
+                    Math.sin(
+                        ((2 * Math.PI) / WAVE_LENGTH) * (cX - ts * WAVE_SPEED)
+                    )
+
+            if (sourceType.value === 'mouse') {
+                const mouseDist = Math.sqrt(
+                    (cX - mouseX.value) ** 2 + (cY - mouseY.value) ** 2
+                )
+
+                // mouse effect
+                size *=
+                    1 +
+                    (MOUSE_WEIGHT - 1) *
+                        (1 - smoothstep(mouseDist / MOUSE_RANGE))
+            }
 
             ctx.beginPath()
             ctx.ellipse(cX, cY, size, size, 0, 0, 2 * Math.PI)
